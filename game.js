b5db0350ad0a9ea292811b74bea78493ca4c476c
David Omar <davidomartech@tutamail.com>
2026-01-17 09:49:21 -0500

    Add function to play a round of rock,paper,scissors

--- game.js
+++ game.js
@@ -17,3 +17,35 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Choose: rock,paper,scissors");
 }
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log("Tie!");
+    return;
+  }
+
+  const winningOutput = `Victory! ${humanChoice} beats ${computerChoice}.`;
+  const losingOutput = `Defeat! ${computerChoice} beats ${humanChoice}.`;
+  let humanWon = false;
+
+  switch (humanChoice) {
+    case "rock":
+      if (computerChoice === "scissors") humanWon = true;
+      break;
+    case "scissors":
+      if (computerChoice === "paper") humanWon = true;
+      break;
+    case "paper":
+      if (computerChoice === "rock") humanWon = true;
+      break;
+  }
+
+  if (humanWon) {
+    humanScore++;
+    console.log(winningOutput);
+  } else {
+    computerScore++;
+    console.log(losingOutput);
+  }
+}
